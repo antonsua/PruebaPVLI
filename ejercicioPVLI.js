036cef4bb6c26307520f1a40052a987547572128
Avy <Avy>
2016-10-07 12:03:36 +0200

    ejercicio1PVLI

---
+++ ejercicioPVLI.js
@@ -0,0 +1,95 @@
+function getNextAliveEnemy(){
+var nextEnemy;
+if(aliveEnemies.lenght>0){
+  nextEnemy = aliveEnemies[0];
+  }
+else{
+  nextEnemy = null;
+  }
+return nextEnemy;
+}
+var hero = {sword:null, shield:null};
+hero.sword = {attack:20, magic:5};
+hero.sword = null;
+var hero ={
+name 'Link';
+var hero ={
+name:'Link,
+var hero ={
+name:'Link',
+life:100,
+weapon: (kind:'sword', power:20, magicPower:5),
+defense:(kind:'shield', power:20, magicPower:0),
+inventory: {
+  {item:null, count:0},
+  {item:null, count:0},
+  {item: {kind:'potion', power:15}, count:5}
+  }
+};
+hero;
+hero = {
+name:'Link',
+life:100,
+weapon:{kind:'sword', power:20, magicPower:5},
+defense: {kind: 'shield', power:5, magicPower:0},
+inventory:[
+{item: null, count:0},
+{item: null, count:0},
+{item:{kind: 'potion', power:15}, count:5}
+]
+};
+hero.name;
+hero.weapon.kind;
+hero.inventory[0];
+hero.inventory[0].item;
+hero.inventory[2].item.power;
+var enemy ={
+graphic: 'specie01.png',
+currentDirection: 'right',
+position:{x:10, y:10},
+score:40
+};
+enemy = _graphic: 'specie01.png',
+enemy = {
+_graphic: 'specie.png',
+_currentDirection: 'right',
+_position: {x:10, y:10},
+_socre:40,
+moveLeft: fuction(){console.log('Going left!');},
+moveRight: function(){console.log('Going right!');},
+advance: function(){console.log('Marching forward!');},
+shoot: function() {console.log('RATATATATATA!');}
+};
+enemy;
+var enemy = {
+_graphic: 'specie01.png',
+enemy = _graphic: 'specie01.png',
+_currentDirection: 'right',
+_position:{x:10, y:10},
+_socre:40,
+moveLeft: function(){console.log('Going Left!');},
+moveRight: function(){console.log('Going right!');},
+advance: function(){console.log('Marching forward!');},
+shoot:function () {console.log('RATATATATA!');}
+};
+enemy = {
+_graphic: 'specie01.png',
+_currentDirection: 'right',
+_position:{x:10, y:10},
+_score:40,
+moveLeft: function(){console.log('Going Left!');},
+moveRight: function(){console.log('Going right!');},
+advance: function(){console.log('Marching forward!');},
+shoot:function () {console.log('RATATATATA!');}
+};
+hero;
+enemy;
+enemy.shoot();
+enemy.moveLeft();
+enemy.advance();
+enemy['shoot']();
+enemy.shoot = function(){console.log('BANG!');};
+enemy.shoot();
+enemy;
+enemy.moveLeft();
+enemy;
